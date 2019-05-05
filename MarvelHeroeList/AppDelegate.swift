1343e8c655e6f2352a55af42ea837ae9d6ddc9ba
Pedro Lopes <>
2019-05-05 05:16:48 -0300

    Added random chance to capture pokemon

--- MarvelHeroeList/AppDelegate.swift
+++ MarvelHeroeList/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import alamofire
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- MarvelHeroeList/ViewController.swift
+++ MarvelHeroeList/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import alamofire
 
 class ViewController: UIViewController {
 
